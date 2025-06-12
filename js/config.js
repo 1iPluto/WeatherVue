0f3f3e99aaf577f97f73a129f883f9d6bd03870b
Marwan <marwansaid0114@gmail.com>
2025-06-12 17:40:55 +0300

    Add configuration file with API settings

---
+++ js/config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+    API_KEY: '67be162fdc308ec6c85869e3ac633f20',
+    BASE_URL: 'https://api.openweathermap.org/data/2.5',
+    ICON_URL: 'https://openweathermap.org/img/wn/',
+    UNITS: 'metric',
+    MAX_RECENT_SEARCHES: 5,
+    STORAGE_KEYS: {
+        RECENT_SEARCHES: 'weatherapp_recent_searches',
+        THEME: 'weatherapp_theme'
+    }
+}; 
